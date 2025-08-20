c3eb61c11eebe3f76cf618a543c4cf2e41317dda
mhdrdn122 <mhdrdn122@gmail.com>
2025-08-20 10:28:31 +0300

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
+ 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  
+  plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        sw: './public/firebase-messaging-sw.js',
+      rollupOptions: {
+          input: {
+              main: './index.html',
+              sw: './public/firebase-messaging-sw.js',
+          },
       },
-    },
-    // ⬇️ أضف هذا
-    assetsInlineLimit: 4096 // 4kb
   },
 
   server: {
-    host: true,
-    port: 5173,
+      host: true,
+      port: 5173,
   },
-})
+});
