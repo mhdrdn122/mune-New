165da42daa6fca5da1d2126d281557bc9bbe4ef4
mhdrdn122 <mhdrdn122@gmail.com>
2025-08-20 09:39:22 +0300

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
-
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        sw: './public/firebase-messaging-sw.js',
-      },
-    },
-    // ⬇️ أضف هذا
-    assetsInlineLimit: 4096 // 4kb
+    // ⬇️ احذف rollupOptions إذا لم تكن ضرورية
+    assetsInlineLimit: 4096
   },
 
   server: {
